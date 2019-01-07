c17653870ab6a0d14b77f0d50b095fbb078cfa8e
Brad Wilson <dotnetguy@gmail.com>
2019-01-07 10:00:47 -0800

    WIP DO NOT MERGE

---
+++ build/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using SimpleExec;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+internal class Program
+{
+    public static Task Main(string[] args)
+    {
+        Target("default", DependsOn("pack", "test"));
+
+        Target("build", () => RunAsync("dotnet", "build --configuration Release"));
+
+        Target(
+            "pack",
+            // DependsOn("build"),
+            // ForEach("LiteGuard.nuspec", "LiteGuard.Source.nuspec"),
+            // async nuspec =>
+            // {
+            //     Environment.SetEnvironmentVariable("NUSPEC_FILE", nuspec, EnvironmentVariableTarget.Process);
+            //     await RunAsync("dotnet", $"pack src/LiteGuard --configuration Release --no-build");
+            // }
+        );
+
+        Target(
+            "test",
+            DependsOn("build"),
+            () => RunAsync("dotnet", $"test --configuration Release --no-build")
+        );
+
+        return RunTargetsAndExitAsync(args, ex => ex is NonZeroExitCodeException);
+    }
+}
